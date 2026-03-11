d0a72001019d254bd18cd47b14c0ba1849bd900e
Ephraim Asad <zainasad98@gmail.com>
2026-03-11 19:30:41 +0000

    CI fixes

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -22,6 +22,8 @@ export default defineConfig({
     command: 'npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
+    timeout: 180 * 1000,
+    stdout: 'pipe',
+    stderr: 'pipe',
   },
 });
